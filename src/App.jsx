4602531fe979fc094b3121c6f312e84d225e7b67
ysharma-dev <yugam.sharma@yahoo.com>
2024-04-18 00:13:25 -0700

    First React custom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+function Header() {
+  return (
+    <header>
+      <img
+        src="/react-essentials-app/assets/images/react-core-concepts.png"
+        alt="Stylized atom"
+      />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img
-          src="/react-essentials-app/assets/images/react-core-concepts.png"
-          alt="Stylized atom"
-        />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
