0f8ede28054694c954055f007c2f576104f4a57f
ysharma-dev <yugam.sharma@yahoo.com>
2024-04-24 21:03:18 -0700

    Forwarding props to wrapped elements

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,15 +1,15 @@
 import CoreConcept from "./CoreConcept";
 import { CORE_CONCEPTS } from "../data";
+import Section from "./Section";
 
 export default function CoreConcepts() {
   return (
-    <section id="core-concepts">
-      <h2>Core Concepts</h2>
+    <Section id="core-concepts" title="Core Concepts">
       <ul>
         {CORE_CONCEPTS.map((conceptItem) => (
           <CoreConcept key={conceptItem.title} {...conceptItem} />
         ))}
       </ul>
-    </section>
+    </Section>
   );
 }
