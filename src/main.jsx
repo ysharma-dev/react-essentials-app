610f662dd753b674d9f6de2f29dee68acf845916
ysharma-dev <yugam.sharma@yahoo.com>
2024-04-24 19:41:19 -0700

    You don't have to use JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
